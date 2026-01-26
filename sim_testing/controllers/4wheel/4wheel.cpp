95278b81ef77bacc5ed55deabefaf81cdf1d527c
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2026-01-25 21:50:41 -0800

    generated adjacency map for A* and fixed 4wheel robot proto

--- sim_testing/controllers/4wheel/4wheel.cpp
+++ sim_testing/controllers/4wheel/4wheel.cpp
@@ -1,10 +1,13 @@
 #include <webots/Robot.hpp>
 #include <webots/Motor.hpp>
 #include <webots/DistanceSensor.hpp>
+#include <webots/Camera.hpp>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TIME_STEP 64
-#define DEFAULT_VEL 3.5
-#define THRESH 950
+#define DEFAULT_VEL 5
+#define THRESH 200
 using namespace webots;
 
 int main(int argc, char **argv) {
@@ -13,6 +16,10 @@ int main(int argc, char **argv) {
   char wheelNames[4][8] = {"wheel1", "wheel2", "wheel3", "wheel4"};
   DistanceSensor *ds[2];
   char dsNames[2][10] = {"ds_right", "ds_left"};
+
+  Camera camera = Camera("camera");
+  camera.enable(TIME_STEP);
+
   
   for (int i = 0; i < 4; i++) {
     wheels[i] = robot->getMotor(wheelNames[i]);
@@ -35,13 +42,18 @@ int main(int argc, char **argv) {
     double leftVels = DEFAULT_VEL;
     double rightVels = DEFAULT_VEL;
     if (counter > 0) {
-      leftVels *= -1;
+      leftVels = DEFAULT_VEL;
+      rightVels = DEFAULT_VEL / 2;
       counter--;
     } else {
-      if (dsVals[0] < THRESH || dsVals[1] < THRESH) {
-        counter = 40;
+      if (dsVals[0] > THRESH || dsVals[1] > THRESH) {
+        counter = 5;
       }
     }
+    
+    printf("dist sensor vals: %.2f and %.2f\n", dsVals[0], dsVals[1]);
+    printf("left wheel velocities: %.2f, %.2f\n", wheels[0]->getVelocity(), wheels[2]->getVelocity());
+    printf("right wheel velocities: %.2f, %.2f\n", wheels[1]->getVelocity(), wheels[3]->getVelocity());
 
     wheels[0]->setVelocity(leftVels);
     wheels[1]->setVelocity(rightVels);
